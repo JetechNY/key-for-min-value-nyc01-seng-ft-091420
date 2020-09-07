9312bc9f0c9b78b5dd03d35a9e4a13eb07695aaa
SunJet Liu <sunjetliu@gmail.com>
2020-09-07 16:26:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
 min_num = 1000
 min_key = ""
 name_hash.each do |a,cost|
-  if cost < min_num
-min_num = cost && min_key = a
+#   if cost < min_num
+# min_num = cost && min_key = a
   end
 end
 puts min_key
